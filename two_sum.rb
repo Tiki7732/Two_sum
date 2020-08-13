9eb284f04d18ed6ebde240b4a616b1153a7f2bb5
tiki7732 <lsaidah@gmail.com>
2020-08-13 12:37:23 -0500

    wrote okay_two_sum

--- two_sum.rb
+++ two_sum.rb
@@ -7,7 +7,31 @@ def bad_two_sum?(array, target_sum)
     false
 end
 # O(n^2) because you are running through the array twice
+# O(1) space
+ 
+def okay_two_sum(array, target_sum)
+    array.sort
+    i = 0
+    j = array.length - 1
+    while i < j
+        case (array[i] + array[j]) <=> target_sum
+        when -1
+            i += 1
+        when 0
+            return true
+        when 1
+            j -= 1
+        end
+    end
+    false
+end
+# O(nlogn) time
+# O(n) space
+
 
 arr = [0, 1, 5, 7]
 p bad_two_sum?(arr, 6) 
 p bad_two_sum?(arr, 10)
+
+p okay_two_sum(arr, 6)
+p okay_two_sum(arr, 10)
